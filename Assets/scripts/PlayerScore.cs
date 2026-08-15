561beaacb28e79daf34f3d652158eb8bb4b3ddcc
cai-a <6nmp8hc4d5@privaterelay.appleid.com>
2026-08-14 20:47:52 -0700

    Add win condition

--- Assets/scripts/PlayerScore.cs
+++ Assets/scripts/PlayerScore.cs
@@ -6,6 +6,8 @@ public class PlayerScore : MonoBehaviour
     public int score = 0;
     public TMP_Text scoreText;
 
+    public TMP_Text winText;
+
     public void AddPoint()
 {
     score += 1;
@@ -13,5 +15,11 @@ public class PlayerScore : MonoBehaviour
     scoreText.text = "Score: " + score;
 
     Debug.Log("Score: " + score);
+
+    if (score==5) 
+    {
+        winText.gameObject.SetActive(true);
+    }
+    
 }
 }
