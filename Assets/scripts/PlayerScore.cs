0bc00ed6a44120cc6e259a876159dd723a63eb72
cai-a <6nmp8hc4d5@privaterelay.appleid.com>
2026-08-16 22:48:23 -0700

    Added Restart function

--- Assets/scripts/PlayerScore.cs
+++ Assets/scripts/PlayerScore.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 public class PlayerScore : MonoBehaviour
 {
     public int score = 0;
     public TMP_Text scoreText;
-
     public TMP_Text winText;
 
     public void AddPoint()
@@ -20,6 +20,13 @@ public class PlayerScore : MonoBehaviour
     {
         winText.gameObject.SetActive(true);
     }
-    
 }
+
+void Update()
+    {
+        if (score == 5 && Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
 }
