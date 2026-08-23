19b4a0e43b9a5aed06944b7d9411672dea085c0f
cai-a <6nmp8hc4d5@privaterelay.appleid.com>
2026-08-23 06:41:56 -0700

    Add enemy prefab and automatic scene references

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -5,10 +5,26 @@ public class Enemy : MonoBehaviour
     public Transform player;
     public GameManager gameManager;
     public float speed = 2f;
+    public float chaseRange = 5f;
     public int damage = 1;
     public float damageCooldown = 1f;
     private float damageTimer = 0f;
+    private Rigidbody2D rb;
 
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+
+        }
+
+        gameManager = FindFirstObjectByType<GameManager>();
+    }
     void Update()
     {
         if (gameManager.currentState != GameManager.GameState.Playing)
@@ -20,14 +36,33 @@ public class Enemy : MonoBehaviour
         {
             damageTimer -= Time.deltaTime;
         }
+    }
 
-        Vector3 direction = player.position - transform.position;
+    void FixedUpdate()
+    {
+        if (gameManager.currentState != GameManager.GameState.Playing)
+        {
+            return;
+        }
 
-        direction = direction.normalized;
+        Vector2 toPlayer = (Vector2)player.position - rb.position;
 
-        transform.position += direction * speed * Time.deltaTime;
+        float distanceToPlayer = toPlayer.magnitude;
 
-        Debug.Log("Timer: " + damageTimer);
+
+
+        if (distanceToPlayer > chaseRange)
+        {
+            return;
+        }
+
+        Vector2 direction = toPlayer.normalized;
+    
+
+            rb.MovePosition(
+            rb.position + direction * speed * Time.fixedDeltaTime
+        );
+    
     }
 
     void OnTriggerStay2D(Collider2D other)
